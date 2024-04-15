45753f3d1a7b03ba23fe91652478f1cd08726e98
JorgePerC <jorgeperez_1@live.com>
2024-04-15 13:32:36 -0600

    added code from middleware and cubemx to compare contents

---
+++ m/inc/SamplePeripheral.h
@@ -0,0 +1,8 @@
+/*
+ * SamplePeripheral.h
+ *
+ *  Created on: Mar 20, 2024
+ *      Author: JorgePerC
+ * 
+ * This is a sample configuration file for a peripheral I2C, CAN, etc.
+ */
---
+++ m/src/SamplePeripheral.c
@@ -0,0 +1,8 @@
+/*
+ * SamplePeripheral.c
+ *
+ *  Created on: Mar 20, 2024
+ *      Author: JorgePerC
+ * 
+ * This is a sample configuration file for a peripheral I2C, CAN, etc.
+ */
